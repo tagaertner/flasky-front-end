8f9bc5f9bbf9ff425b83cdd0be80ac3519e66197
Mikelle <119449245+mikellewade@users.noreply.github.com>
2024-05-10 12:23:24 -0400

    Update src/components/DogList.jsx
    Co-authored-by: Ansel Rognlie <36503186+anselrognlie@users.noreply.github.com>

--- src/components/DogList.jsx
+++ src/components/DogList.jsx
@@ -2,14 +2,14 @@ import Dog from './Dog';
 import './DogList.css';
 
 const DogList = () => {
-  return  (
-    <div>
-      <h2 className='dogListHeader'> Dog List </h2>
+  return (
+    <div className="dogList">
+      <h2> Dog List </h2>
       <Dog />
       <Dog />
       <Dog />
     </div>
-)
+  );
 };
 
 export default DogList;

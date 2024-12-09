f44d1568022c44170cdfc8587ac12041e2be5927
Tami <tamigaertner@outlook.com>
2024-12-09 12:31:30 -0500

    add cat components and set up

---
+++ src/components/CatList.jsx
@@ -0,0 +1,14 @@
+const CatList =()=>{
+  return(
+    <>
+      <h2>Cat List</h2>
+      <ul>
+        <li>
+          <h3></h3>
+        </li>
+        </ul>
+    </>
+  );
+};
+
+export default CatList;
